2da503dbd27fada0f8ada7047d87fee6c34ec234
DESKTOP-0J0G6T3\Roman Mikhailovskikh <romanunylov666@gmail.com>
2023-09-15 11:21:14 +0500

    Create empty web api project

---
+++ MaxTechCS/Controllers/TasksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaxTechCS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        
+    }
+}
